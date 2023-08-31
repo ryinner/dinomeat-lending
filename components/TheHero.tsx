428083f39bf94e9cf5b8e3ead432738057bf8b7b
ryinner <vinograd18062002@gmail.com>
2023-08-31 16:53:01 +0500

    hero ready

--- components/TheHero.tsx
+++ components/TheHero.tsx
@@ -1,11 +1,18 @@
+import { Black_Ops_One } from 'next/font/google';
 import Image from 'next/image';
 import heroImage from '../public/hero-image.jpg';
 import style from './TheHero.module.css';
+
+const blackOpsOne = Black_Ops_One({ weight: '400', subsets: ['latin'] });
 
 export default function TheHero () {
   return (
     <section className={style.hero}>
       <Image className={style.hero__image} src={heroImage} fill={true} alt='Счастливые покупатели' />
+      <div className={style.hero__info}>
+        <div className={`${style.hero__logo} ${blackOpsOne.className}`}>Dinomeät</div>
+        <div className={style.hero__city}>Челябинск</div>
+      </div>
     </section>
   );
 }
