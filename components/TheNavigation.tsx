1ded08ca52fe9a9842f130fb1b081ee5f2bce5b9
ryinner <vinograd18062002@gmail.com>
2023-08-31 10:15:59 +0500

    mobile navigation

--- components/TheNavigation.tsx
+++ components/TheNavigation.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from './TheNavigation.module.css';
 
 const linksMap = [
@@ -8,10 +11,16 @@ const linksMap = [
 ];
 
 export default function TheNavigation () {
+  const [isActive, setIsActive] = useState(false);
+
+  const burgerMenuHandler = () => { setIsActive(() => !isActive); };
+
   return (
-    <nav>
-      <ul className={styles.navigation}>
-        { linksMap.map(link => <li key={link.label} className={styles.link}><Link href={link.link}>{ link.label }</Link></li>) }
+    <nav className={styles.navigation}>
+      <span className={styles.navigationBurger} title='Навигация' onClick={burgerMenuHandler} />
+      <ul className={`${styles.navigationList} ${isActive && styles.navigationListActive}`}>
+        <li><span className={styles.navigationBurger} title='Навигация' onClick={burgerMenuHandler} /></li>
+        {linksMap.map(link => <li key={link.label} className={styles.link}><Link href={link.link}>{link.label}</Link></li>)}
       </ul>
     </nav>
   );
